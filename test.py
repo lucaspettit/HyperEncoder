f7d9e1c42f19081ad1e8846dc527d1ac72311389
Lucas Pettit <lucas.pettit@hp.com>
2018-09-28 15:03:28 -0700

    removing old files

--- test.py
+++
@@ -1,8 +0,0 @@
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-
-parser.add_argument('--x_dim', nargs='+', type=int, default=[10, 20, 30], help='add ints')
-args = parser.parse_args()
-
-print(args)
